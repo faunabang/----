eea3c0d9018aea8f8380a6a7941da2b9b7647acd
faunabang <142366077+faunabang@users.noreply.github.com>
2024-04-30 20:09:34 +0900

    d

--- 4) 정렬/5-1. 힙정렬.py
+++ 4) 정렬/5-1. 힙정렬.py
@@ -3,3 +3,12 @@ li=[1,6,2,8,9,3,0,5,4,7]
 heapq.heapify(li)
 sorted=[heapq.heappop(li) for i in range(len(li))]
 print(sorted)
+
+# import heapq
+# v=[23,12,16,13,32,8,2,24,47,26]
+# heapq.heapify(v)
+# print(v)
+# sorted = []
+# while v:
+#   sorted.append(heapq.heappop(v))
+# print(sorted)
